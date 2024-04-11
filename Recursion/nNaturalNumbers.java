436c5f81cd43e9a2d1dee00c66df7e068ba97429
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-04-11 22:29:26 +0530

    Recursion N Natural Number

---
+++ Recursion/nNaturalNumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class nNaturalNumbers {
+    static void nNaturalNum(int n) {
+        if (n > 1)
+            nNaturalNum(n - 1);
+        System.out.printf(n+" ");
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.print("Natural Numbers = ");
+        nNaturalNum(n);
+    }
+}
