c26956a70f1d840bada7b82dcf3f9f7aa064963f
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-04-11 16:50:33 +0530

    Recursion in factorial

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -1,6 +1,17 @@
+import java.util.*;
 public class factorial {
 
     public static void main(String[] args){
-        System.out.println("huuh");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+        int k = fact(n);
+        System.out.println(n+" factorial is "+k);
+    }
+    static int fact(int n){
+        if(n==0)
+        return 1;
+        int s = n * fact(n-1);
+        return s;
     }
 }
