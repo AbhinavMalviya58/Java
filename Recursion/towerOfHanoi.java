b77b39668c34c14b97488a288ef6b06ec2c4004f
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-04-13 16:39:22 +0530

    Recursion Question

--- Recursion/towerOfHanoi.java
+++ Recursion/towerOfHanoi.java
@@ -14,9 +14,25 @@ import java.util.*;
                 A                               B                                       C
 */
 public class towerOfHanoi {
+    static void tower(int n, String t1,String t2,String t3){
+        if(n==0){
+            return;
+        }
+
+        tower(n-1, t1, t3, t2);
+        System.out.println(n+"["+t1+"=>"+t2+"]");
+        tower(n-1, t3, t2, t1);
+
+    }
 
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        String a = scn.next();
+        String b = scn.next();
+        String c = scn.next();
+        scn.close();
+        tower(n, a, b, c);
 
     }
 }
