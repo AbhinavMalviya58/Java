337109250aac277b5b3e6c9add504d787d478d5b
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-04-14 14:51:51 +0530

    Recursion

--- Recursion/displayAnArray.java
+++ Recursion/displayAnArray.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class displayAnArray {
-    static void Array(int a[], int i) {
-        for (i = 0; i <= a.length - 1; i++) {
-            System.out.println(a[i]);
-        }
+    static void Array(int a[], int index) {
+        if(index == a.length)
+        return;
+        System.out.println(a[index]);
+        Array(a, index+1);
     }
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        scn.close();
+        int [] a = {1,2,3,4,5,6,7};
+        Array(a,0);
     }
 }
