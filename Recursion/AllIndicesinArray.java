7114ce38c6badcf68b956e6fcc660823db8d54d5
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-04-17 00:42:27 +0530

    update

--- Recursion/AllIndicesinArray.java
+++ Recursion/AllIndicesinArray.java
@@ -1,13 +1,26 @@
 public class AllIndicesinArray {
-    static int  allIndex(int a[], int idx, int data){
+    static int[]  allIndex(int a[], int idx, int data,int fsf){
         if(idx == a.length){
-            return -1;
+            return new int[fsf];
         }
-        int index = allIndex(a, idx+1, data);
+        if(a[idx]==data){
+            int[] index = allIndex(a, idx+1, data, fsf + 1);
+            index[fsf] = idx;
+            return index; 
+        }else{
+            int[] index = allIndex(a, idx+1, data, fsf);
+            return index;
+        }
     }
     public static void main(String[] args) {
         int[] num = {3,4,5,3,2,3,};
-        int ans = allIndex(num, 0, 3);
-        System.out.println(ans);
+        int[] ans = allIndex(num, 0, 3,0);
+        if(ans.length == 0){
+            System.out.println();
+            return;
+        }
+        for(int i = 0;i<=ans.length-1;i++){
+            System.out.print(ans[i]+" ");
+        }
     }
 }
