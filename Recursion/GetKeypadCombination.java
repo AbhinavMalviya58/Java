d35347c5aa95c3b40aa3afbb8e13969c624f19e0
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-04-21 21:15:27 +0530

    update

---
+++ Recursion/GetKeypadCombination.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class GetKeypadCombination {
+    String[] code = {"abc" ,"def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+    static ArrayList<String> grtKeypad(String key){
+        char ch = key.charAt(0);
+        String Skey = key.substring(1);
+        ArrayList<String> mres = grtKeypad(key);
+        
+        ArrayList<String> newArr = new ArrayList<>();
+        for(int i = 0;i<= newArr.size(); i++){
+            
+        }
+
+    }
+}
--- Recursion/GetSubsequence.java
+++ Recursion/GetSubsequence.java
@@ -1,3 +1,28 @@
+import java.util.*;
 public class GetSubsequence {
-    
+    static ArrayList<String> Subsequence(String str){
+        if(str.length()==0){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        char ch = str.charAt(0);//a
+        String sub = str.substring(1);//bc
+        ArrayList<String> fath = Subsequence(sub);
+
+        ArrayList<String> newArr = new ArrayList<>();
+        for(String n : fath){
+            newArr.add("_"+n);
+        }
+        for(String n : fath){
+            newArr.add(ch+n);
+        }
+        
+        return newArr;
+    }
+    public static void main(String[] args) {
+        ArrayList<String> ans = Subsequence("abc");
+        System.out.println(ans);
+    }
 }
