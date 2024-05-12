4ae75458939cc78534cabb34beddcd1a76dc7086
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-13 00:02:55 +0530

    add print keypad combination update

--- Recursion/printKeypadCombination.java
+++ Recursion/printKeypadCombination.java
@@ -1,6 +1,23 @@
 public class printKeypadCombination {
-    static String[] code = {",;","abc" ,"def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    static String[] code = {" ",",;","abc" ,"def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     static void printKeypad(String ques, String Ans){
+        if(ques.length() == 0){
+            System.out.print(Ans + " ");
+            return;
+        }
         
+        char ch = ques.charAt(0);
+        String str = ques.substring(1);
+
+        String codeForCh = code[ch - '0'];
+
+        for(int i = 0; i < codeForCh.length(); i++){
+            char cho = codeForCh.charAt(i);
+            printKeypad(str, Ans + cho);
+        }
+    }
+    public static void main(String[] args) {
+        String k = "23";
+        printKeypad(k, "");
     }
 }
