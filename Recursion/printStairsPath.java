129fe58f1df72c0a9dc0429d6a0f253e8b469872
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-14 01:58:18 +0530

    print stairs path

---
+++ Recursion/printStairsPath.java
@@ -0,0 +1,19 @@
+public class printStairsPath {
+    
+    public static void printStairs(int stair, String path){
+        if(stair < 0){
+            return;
+        }
+        if(stair == 0){
+            System.out.print(path+" ");
+            return;
+        }
+
+        printStairs(stair - 1, path + "1");
+        printStairs(stair - 2, path + "2");
+        printStairs(stair - 3, path + "3");
+    }
+    public static void main(String[] args) {
+        printStairs(4," ");
+    }
+}
