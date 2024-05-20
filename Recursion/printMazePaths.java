68ce965eaec99e59e4199623d545749e339d7142
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-20 21:08:12 +0530

    print Maze paths update

--- Recursion/printMazePaths.java
+++ Recursion/printMazePaths.java
@@ -1,12 +1,22 @@
 public class printMazePaths {
-    public static void mazePaths(int startH, int startV, int endH, int endV, String psf){
+    public static void mazePaths(int startR, int startC, int endR, int endC, String psf){
+        if(startR == endR && startC == endC){
+            System.out.print(psf);
+            return;
+        }
     
-        for(int ms = 1 ; ms <= endH - startH; ms++){
-            mazePaths(startH, startV, endH, endV, "H");
+        for(int ms = 1 ; ms <= endR - startR; ms++){
+            mazePaths(startR + ms, startC, endR, endC, psf + "H" + ms);
+        }
+        for(int ms = 1 ; ms <= endC - startC; ms++){
+            mazePaths(startR, startC + ms, endR, endC, psf + "V" + ms);
+        }
+        for(int ms = 1; ms <= endR - startR && ms <= endC - startC; ms++){
+            mazePaths(startR + ms, startC + ms, endR, endC, psf + "D" + ms);
         }
     }
     public static void main(String[] args){
-        mazePaths(0, 0, 4, 4, null);
+        mazePaths(0, 0, 3, 3, " ");
     }
 }
 
