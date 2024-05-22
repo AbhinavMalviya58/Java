e94d089145d327b060e729736c8f705af579ba7d
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-23 03:29:04 +0530

    modified

--- Recursion/printEncodings.java
+++ Recursion/printEncodings.java
@@ -10,14 +10,34 @@ public class printEncodings {
 
     public static void Encodings(String str, String ans){
         if(str.length() == 0){
-            System.out.print(ans);
+            System.out.println(ans);
             return;
         }else if(str.length() == 1){
             char ch = str.charAt(0);
             if(ch == '0'){
                 return;
             }else{
-                
+                int cvh = ch - '0';
+                char code = (char)('a' + cvh - 1);
+                System.out.println(ans + code);
+            }
+        }
+        else {
+            char ch = str.charAt(0);
+            String ros = str.substring(1);
+            if(ch == '0'){
+                return;
+            }else{  
+                int cvh = ch - '0';
+                char code = (char)('a' + cvh - 1);
+                Encodings(ros, ans + code);
+            }
+            String ch12 = str.substring(0,2);
+            String roq12 = str.substring(2);
+            int ch12v = Integer.parseInt(ch12);
+            if(ch12v <= 26){
+                char code = (char)('a' + ch12v - 1);
+                Encodings(roq12, ans + code);
             }
 
         }
