92a5279de57d03e44f612ec37857d0a57115881f
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-24 20:41:32 +0530

    rechecking code

--- Recursion/printEncodings.java
+++ Recursion/printEncodings.java
@@ -1,42 +1,41 @@
 import java.util.Scanner;
 
 public class printEncodings {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String str = scn.next();
         scn.close();
         Encodings(str, "");
     }
 
-    public static void Encodings(String str, String ans){
-        if(str.length() == 0){
+    public static void Encodings(String str, String ans) {
+        if (str.length() == 0) {
             System.out.println(ans);
             return;
-        }else if(str.length() == 1){
+        } else if (str.length() == 1) {
             char ch = str.charAt(0);
-            if(ch == '0'){
+            if (ch == '0') {
                 return;
-            }else{
+            } else {
                 int cvh = ch - '0';
-                char code = (char)('a' + cvh - 1);
+                char code = (char) ('a' + cvh - 1);
                 System.out.println(ans + code);
             }
-        }
-        else {
+        } else {
             char ch = str.charAt(0);
             String ros = str.substring(1);
-            if(ch == '0'){
+            if (ch == '0') {
                 return;
-            }else{  
+            } else {
                 int cvh = ch - '0';
-                char code = (char)('a' + cvh - 1);
+                char code = (char) ('a' + cvh - 1);
                 Encodings(ros, ans + code);
             }
-            String ch12 = str.substring(0,2);
+            String ch12 = str.substring(0, 2);
             String roq12 = str.substring(2);
             int ch12v = Integer.parseInt(ch12);
-            if(ch12v <= 26){
-                char code = (char)('a' + ch12v - 1);
+            if (ch12v <= 26) {
+                char code = (char) ('a' + ch12v - 1);
                 Encodings(roq12, ans + code);
             }
 
