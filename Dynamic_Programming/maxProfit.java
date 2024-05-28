f7f450f5b3a577301f2eb8ced62915f57844c5a8
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-29 01:49:04 +0530

    dp ques

---
+++ Dynamic_Programming/maxProfit.java
@@ -0,0 +1,12 @@
+/**
+ * maxProfit Knapsack Recursive
+ */
+public class maxProfit {
+
+    static int profit(int[] value, int[] price, int wate, int arrSize){
+        if(n = arrSize == 0 || value == 0){
+            return 0;
+        }
+        profit(value, price, wate, arrSize)
+    }
+}
