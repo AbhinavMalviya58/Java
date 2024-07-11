598fa75ce105204143f8db8dc1de3b379f199117
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-07-11 20:39:40 +0530

    0_1_knapsack

--- Dynamic_Programming/0_1_Knapsack/maxProfit.java
+++ Dynamic_Programming/0_1_Knapsack/maxProfit.java
@@ -4,7 +4,7 @@
 public class maxProfit {
 
     static int profit(int[] value, int[] price, int wate, int arrSize){
-        if(arrSize == 0 || value == 0){
+        if(arrSize){
             return 0;
         }
         profit(value, price, wate, arrSize);
