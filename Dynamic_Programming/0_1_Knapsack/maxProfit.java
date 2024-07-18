7571a484982eebb6a7b063bee74e1ac04d6a9ec8
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-07-18 22:50:48 +0530

    DP 0-1 knapsack

--- Dynamic_Programming/0_1_Knapsack/maxProfit.java
+++ Dynamic_Programming/0_1_Knapsack/maxProfit.java
@@ -1,12 +1,29 @@
 /**
  * maxProfit Knapsack Recursive
  */
+import java.util.*;
+import java.math.*;
 public class maxProfit {
+    public static void main(String[] args) {
+        // profit()
+    }
 
-    static int profit(int[] value, int[] price, int wate, int arrSize){
-        if(arrSize){
+    int profit(int value[], int wate[], int w, int n){
+        /*
+            item1 = w1 
+            w1 <= w ,  include in bag // or // not include in bag
+            w1 > w      not include in bag
+         */
+        
+        // base condition
+        if (n == 0  || w == 0) {
             return 0;
         }
-        profit(value, price, wate, arrSize);
+        // choice condition
+        if(wate[n-1] <= w){
+            return Math.max(value[n - 1] + profit(value, wate, w - wate[n - 1], n - 1) , profit(value, wate, w, n - 1));
+        }else{
+            return profit(value, wate, w, n - 1);
+        }
     }
 }
