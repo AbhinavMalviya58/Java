524075e7de2bafe773855fde3c5c300a0e21df9b
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-07-22 12:12:10 +0530

    0_1_Knapsack memoization

--- Dynamic_Programming/0_1_Knapsack/memoization.java
+++ Dynamic_Programming/0_1_Knapsack/memoization.java
@@ -1,29 +1,31 @@
 
 public class memoization {
-    public static int t[102];
-    public static void main(String args[]){
-        memset(t, -1, sizeOf(t));
+    public static void main(String args[]) {
 
-        int[] value = {60, 100, 120}; // Values of the items
-        int[] wate = {10, 20, 30};   // Weights of the items
-        int w = 50;                  // Capacity of the knapsack
-        int n = value.length;        // Number of items
+        int[] value = { 60, 100, 120 }; // Values of the items
+        int[] wate = { 10, 20, 30 }; // Weights of the items
+        int w = 50; // Capacity of the knapsack
+        int n = value.length; // Number of items
 
         maxProfit knapsack = new maxProfit();
         int maxProfit = knapsack.profit(value, wate, w, n);
         System.out.println("Maximum profit: " + maxProfit);
     }
-    int maxProfitmemoizetion(int value[], int wate[], int w, int n){
-        if (n == 0  || w == 0) {
+
+    int maxProfitmemoizetion(int value[], int wate[], int w, int n, int[][] memo) {
+        if (n == 0 || w == 0) {
             return 0;
         }
-        if()
+        if (memo[w][n] != -1) {
+            return memo[w][n];
+        }
         // choice condition
-        if(wate[n-1] <= w){
-            return Math.max(value[n - 1] + maxProfitmemoizetion(value, wate, w - wate[n - 1], n - 1) ,
-            maxProfitmemoizetion(value, wate, w, n - 1));
-        }else{
-            return maxProfitmemoizetion(value, wate, w, n - 1);
+        if (wate[n - 1] <= w) {
+            return memo[w][n] =  Math.max(value[n - 1] + maxProfitmemoizetion(value, wate, w - wate[n - 1], n - 1, memo),
+                    maxProfitmemoizetion(value, wate, w, n - 1, memo));
+        } else {
+            return memo[w][n] =  maxProfitmemoizetion(value, wate, w, n - 1, memo);
+        }
+
     }
-    
 }
