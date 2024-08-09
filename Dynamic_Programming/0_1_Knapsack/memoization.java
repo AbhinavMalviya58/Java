75822da40b33a21692b48f580354151c9dc11269
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-08-09 21:44:57 +0530

    memoization

--- Dynamic_Programming/0_1_Knapsack/memoization.java
+++ Dynamic_Programming/0_1_Knapsack/memoization.java
@@ -1,16 +1,16 @@
 
 public class memoization {
-    public static void main(String args[]) {
+    // public static void main(String args[]) {
 
-        int[] value = { 60, 100, 120 }; // Values of the items
-        int[] wate = { 10, 20, 30 }; // Weights of the items
-        int w = 50; // Capacity of the knapsack
-        int n = value.length; // Number of items
+    //     int[] value = { 60, 100, 120 }; // Values of the items
+    //     int[] wate = { 10, 20, 30 }; // Weights of the items
+    //     int w = 50; // Capacity of the knapsack
+    //     int n = value.length; // Number of items
 
-        maxProfit knapsack = new maxProfit();
-        int maxProfit = knapsack.profit(value, wate, w, n);
-        System.out.println("Maximum profit: " + maxProfit);
-    }
+    //     maxProfit knapsack = new maxProfit();
+    //     int maxProfit = knapsack.profit(value, wate, w, n);n
+    //     System.out.println("Maximum profit: " + maxProfit);
+    // }
 
     int maxProfitmemoizetion(int value[], int wate[], int w, int n, int[][] memo) {
         if (n == 0 || w == 0) {
@@ -19,6 +19,7 @@ public class memoization {
         if (memo[w][n] != -1) {
             return memo[w][n];
         }
+        
         // choice condition
         if (wate[n - 1] <= w) {
             return memo[w][n] =  Math.max(value[n - 1] + maxProfitmemoizetion(value, wate, w - wate[n - 1], n - 1, memo),
@@ -26,6 +27,5 @@ public class memoization {
         } else {
             return memo[w][n] =  maxProfitmemoizetion(value, wate, w, n - 1, memo);
         }
-
     }
 }
