da768c34e03aba4db74c8688fcdbd076e09341e2
Abhinav Malviya <abhinavmalviya4321@gmail.com>
2025-08-03 23:40:17 +0530

    add exp

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 class Test{
 	public static void main(String[] args){
-		System.out.print("hello");
+		System.out.print("hello \n");
+		System.out.print(args[0]);
 	}
 }
---
+++ java restart/Java restart/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+        System.out.println(args[0]);
+
+    }
+}
